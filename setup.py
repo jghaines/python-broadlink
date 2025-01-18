f9c8138412b7d0fa8c220d91a4af6486ad8dbf5f
Jason Haines <git@jhaines.name>
2025-01-18 12:33:41 +0930

    Add broadlink_cli to deployment package Allow JSON (default) output on all commands Add --learnfile template Fix majority of linter warnings

--- setup.py
+++ setup.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Setup script for the python-broadlink package.
 
+This script defines the installation requirements and metadata for the package.
+To install the package, run:
+    python setup.py install
+"""
 
 from setuptools import setup, find_packages
 
 
-version = '0.19.0'
+VERSION = '0.20.0'
 
 setup(
     name="broadlink",
-    version=version,
+    version=VERSION,
     author="Matthew Garrett",
     author_email="mjg59@srcf.ucam.org",
     url="http://github.com/mjg59/python-broadlink",
     packages=find_packages(),
-    scripts=[],
+    scripts=["cli/broadlink_cli"],
     install_requires=["cryptography>=3.2"],
     description="Python API for controlling Broadlink devices",
     classifiers=[
@@ -26,4 +32,9 @@ setup(
     ],
     include_package_data=True,
     zip_safe=False,
+    entry_points={
+        "console_scripts": [
+            "broadlink-cli=broadlink.cli.broadlink_cli:main",
+        ],
+    },
 )
